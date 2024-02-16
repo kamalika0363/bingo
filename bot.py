aa261fa77352bc7eea6f8a4ef43c0c4cfe042a7d
vish-rt <rawatvishal1707@gmail.com>
2024-02-16 21:14:22 +0530

    [ADD] Placeholder for Telegram bot

---
+++ bot.py
@@ -0,0 +1,24 @@
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+# from credentials import bot_token, bot_user_name, URL
+from dotenv import load_dotenv
+
+global bot
+global TOKEN
+
+load_dotenv()
+TOKEN = os.environ.get("BOT_TOKEN")
+
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+
+# async def conn(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+#     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+
+
+app = ApplicationBuilder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("hello", hello))
+
+app.run_polling()
